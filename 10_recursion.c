2f9c8c337f543c9cdbd7f2e60bd14edba23c27f0
yash2003bisht <yash2003bisht@gmail.com>
2022-09-19 07:10:33 +0530

    recursion

---
+++ 10_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+long int fact(int n){
+    if (n == 0 || n == 1){
+        return 1;
+    }
+    return n * fact(n-1);
+}
+
+int main(){
+    // ---------------------- RECURSION ----------------------
+    // - Recursion functions or recursion is a process when a function calls a copy of itself to work on a smaller problem.
+    // - Any function which called itself is called recursion function.
+    // - There is always a base condition in recursive function to terminate the execution.
+    // - Any problem that can solved recursively, can also be solved iteratively.
+
+    // ---------------------- CODE EXAMPLE ----------------------
+
+    long int f = fact(5);
+    printf("%ld\n", f);
+
+}
