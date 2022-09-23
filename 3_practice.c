2a67ec5796b1116d34b03ad1058c531b57ffbb0d
yash2003bisht <yash2003bisht@gmail.com>
2022-09-23 12:31:13 +0530

    fibonacci programme using recursion & iteration

---
+++ 3_practice.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <time.h>
+
+
+/*
+    ---------------- Fibonacci Series ----------------
+    In mathematics, the Fibonacci numbers, commonly denoted Fₙ, form a sequence, the Fibonacci sequence,
+    in which each number is the sum of the two preceding ones. The sequence commonly starts from 0 and 1,
+    although some authors omit the initial terms and start the sequence from 1 and 1 or from 1 and 2.
+
+    ----------------> In this programme fibonacci sequence start from 1 and 2 <----------------
+*/
+
+
+int fib_r(int n){
+    if (n==0 || n==1){
+        return 1;
+    } else{
+        return fib_r(n-1) + fib_r(n-2);
+    }
+}
+
+
+int fib_i(int n){
+    int a = 1;
+    int b = 2;
+    int c;
+    if (n==1){
+        return a;
+    } else if (n==2){
+        return b;
+    } else{
+        for (int i=1; i<n-1; i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        return c;   
+    }
+}
+
+
+int main(){
+
+    // fibonacci using recursion
+    clock_t t_r;
+    t_r = clock();
+    printf("The fibonacci of 45 using recursion is %d\n", fib_r(45));
+    t_r = clock() - t_r;
+    double time_r = ((double)t_r)/CLOCKS_PER_SEC;
+    printf("Time taken to calculate the fibonacci series using recursion %f\n", time_r);
+    
+    printf("\n");
+
+    // fibonacci using iteration
+    clock_t t_i;
+    t_i = clock();
+    printf("The fibonacci of 45 using iteration is %d\n", fib_i(45));
+    t_i = clock() - t_i;
+    double time_i = ((double)t_i)/CLOCKS_PER_SEC;
+    printf("Time taken to calculate the fibonacci series using iteration %f\n", time_i);
+
+    return 0;
+}
