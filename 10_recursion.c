de3a4bf3538991cf53d7c2af44771c6236c61801
yash2003bisht <yash2003bisht@gmail.com>
2022-09-27 07:24:04 +0530

    additional documentation added

--- 10_recursion.c
+++ 10_recursion.c
@@ -13,9 +13,19 @@ int main(){
     // - Any function which called itself is called recursion function.
     // - There is always a base condition in recursive function to terminate the execution.
     // - Any problem that can solved recursively, can also be solved iteratively.
+    
+    
+    // ---------------------- IN A NUT SHELL ----------------------
+    // - Recursins is a good approach when it comes to problem solving
+    // - However, programmer needs to evaluate the need amd impact of using recursive/iterative
+    //   approach while solving a particular problem
+    // - In case of factorial calculation, recursion saved a lot of lines of code.
+    // - However in case of Fibonacci series, recursion resulted in some extra
+    //   unnecessary function cals which was an extra overhead!
+    // - Running time for fibonnacci series grows exponentially with input case of recursive approach!
+    
 
     // ---------------------- CODE EXAMPLE ----------------------
-
     long int f = fact(5);
     printf("%ld\n", f);
 
