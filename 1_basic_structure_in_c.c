2eac0448faec88b0eac0bf52e9aad53f9541e9a3
yash2003bisht <yash2003bisht@gmail.com>
2022-10-04 18:24:32 +0530

    formatting change

--- 1_basic_structure_in_c.c
+++ 1_basic_structure_in_c.c
@@ -1,18 +1,20 @@
 #include <stdio.h> // preprocessor command
+
 // macro definition
 #define LIMIT 5
+
 
 int main(){ // <- programme execution start from here
     printf("Namaste Duniya\n");  // <- function of a programme
     
     /* Steps to convert a c programme into executable file */
     
-    // * 1) preprocessing
-    	/* programme comments removed, marco are being expanded and content of
-    	   #include files copied in a different file */
-    	// marco: Macros are pieces of code in a program that is given some name. 
-    	//        Whenever this name is encountered by the compiler, the compiler 
-    	//	  replaces the name with the actual piece of code.
+    // 1) preprocessing
+    /* programme comments removed, marco are being expanded and content of
+        #include files copied in a different file */
+    // marco: Macros are pieces of code in a program that is given some name. 
+    //        Whenever this name is encountered by the compiler, the compiler 
+    //	  replaces the name with the actual piece of code.
     
     for (int i=0; i<LIMIT; i++){
         printf("%d\n", i);
@@ -24,17 +26,17 @@ int main(){ // <- programme execution start from here
     
     // preprocesse ouput store in a .i file, you can see the content of it.
     
-    // * 2) Assembly
-    	// * In step two .i file converted into assembly level instruction in a .s 
-    	//   file
-    
-    // * 3) Machine
-    	// * Now your assembly instruction converted into machine level 
-    	//   instruction(baniary which is understand by cpu) store in .o file you can 
-    	//   not open .o file because it is in binary formate.
+    // 2) Assembly
+    // In step two .i file converted into assembly level instruction in a .s 
+    // file
+
+    // 3) Machine
+    // Now your assembly instruction converted into machine level 
+    // instruction(baniary which is understand by cpu) store in .o file you can 
+    // not open .o file because it is in binary formate.
     	
-   // * 4) linker
-   	// * This is a final phase, here .o file converted in an executable
+    // 4) linker
+   	// This is a final phase, here .o file converted in an executable
    	
    // To see the full file output run gcc -Wall -save-temps file_name.c 
     	
