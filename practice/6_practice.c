77380b7e30adaa3e4b2ca98a440b38dda40d0811
yash2003bisht <yash2003bisht@gmail.com>
2022-10-07 14:36:58 +0530

    starting and ending spaces removed from the HTML element

--- practice/6_practice.c
+++ practice/6_practice.c
@@ -2,22 +2,39 @@
 #include <string.h>
 
 void parse(char data[]){
-    int i, tag=0, size=strlen(data);
+    int i, tag=0, index=0, size=strlen(data), trailing_spaces=0;
     for (i=0; i<size; i++){
         if (data[i] == '<'){ // tag open
             tag = 1;
+            continue;
         } else if (data[i] == '>'){ // tag close
             tag = 0;
-            data[i] = "";
+            continue;
         }
         
-        // remove tag
-        if (tag){
-            data[i] = "";
+        // remove tag and trailing spaces
+        if(!tag){
+            if (data[i] != ' ' && !trailing_spaces){
+                trailing_spaces = 1;
+            }
+            if (trailing_spaces){
+                data[index] = data[i];
+                index++;
+            }
         }
     }
 
-    printf("%s\n", data);
+    data[index] = '\0';
+
+    // remove ending space
+    size = strlen(data);
+    while(data[size-1] == ' '){
+        data[size-1] = '\0';
+        size--;
+    }
+
+
+    printf("``%s``\n", data);
 }
 
 int main(){
