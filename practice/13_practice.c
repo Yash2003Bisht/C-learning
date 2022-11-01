2187ae722dc56da4a8a618dbab3fdf82be84fa5e
yash2003bisht <yash2003bisht@gmail.com>
2022-11-01 07:34:05 +0530

    command line utility practice

---
+++ practice/13_practice.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const * argv[]) {
+    /*
+        ----------------------- COMMAND LINE ARGUMENT PRACTICE -----------------------
+        - We have to create a command line utility to add/subtract/divide/multiple two numbers
+          first command line argument must be the operation. The next arguments beinf two numbers.
+          For example:
+          >> file_name.c add 50 45
+          >> 95
+    */
+
+    // ----------------------- MAIN CODE -----------------------
+    int a, b;
+    sscanf(argv[2], "%d", &a);
+    sscanf(argv[3], "%d", &b);
+
+    
+    // -------- ADD
+    if (!strcmp(argv[1], "add")) printf("%d\n", a+b);
+    
+    // -------- SUBTRACT
+    else if (!strcmp(argv[1], "sub")) printf("%d\n", a-b);
+
+    // -------- DIVIDE
+    else if (!strcmp(argv[1], "div")) printf("%f\n", (float) a/b);
+
+    // -------- MULTIPLE
+    else if (!strcmp(argv[1], "mul")) printf("%d\n", a*b);
+
+    // -------- INVALID ARGUMENT
+    else printf("Invalid argument\n");
+    
+    // NOTE: strcmp returns 0 when both the strings are equal.
+
+
+
+    return 0;
+}
